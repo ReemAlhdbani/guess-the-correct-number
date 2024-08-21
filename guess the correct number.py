1eb245a8525eb74fc073925025dfc11dc011868a
ReemAlhdbani <148048593+ReemAlhdbani@users.noreply.github.com>
2024-08-21 13:31:58 +0300

    Add files via upload

---
+++ guess the correct number.py
@@ -0,0 +1,9 @@
+
+
+print("guess the correct number !")
+num= int(input("Enter the number :"))
+correct_num = 9
+if num  == 9 :
+    print("you did enter coorect number which is  ",correct_num)
+else:
+    print(f" {num}  is not the correct number \n the coorect number is {correct_num} ")
